7460b7799284fcff9660dfb88c58e73e94144321
gablilli <167802854+gablilli@users.noreply.github.com>
2026-07-10 11:31:54 +0000

    fix: embedding playground url

--- website/src/lib/components/embedding/EmbeddingPlayground.svelte
+++ website/src/lib/components/embedding/EmbeddingPlayground.svelte
@@ -321,7 +321,7 @@
             <pre
                 class="bg-primary text-primary-foreground p-3 rounded-md whitespace-normal break-all">
                 <code class="language-html">
-                    {`<iframe src="https://gpx.studio${base}/embed?options=${encodeURIComponent(JSON.stringify(getCleanedEmbeddingOptions(iframeOptions)))}${hash}" width="100%" height="600px" frameborder="0" style="outline: none;"/>`}
+                    {`<iframe src="https://condottiero.insidegubbio.com${base}/embed?options=${encodeURIComponent(JSON.stringify(getCleanedEmbeddingOptions(iframeOptions)))}${hash}" width="100%" height="600px" frameborder="0" style="outline: none;"/>`}
                 </code>
             </pre>
         </fieldset>
