4c39b242981babaf88ebbbcb5997f89287a927dc
gablilli <167802854+gablilli@users.noreply.github.com>
2026-08-01 22:26:13 +0200

    feat: do not show controls (#6)
    * feat: add showControls option to embedding options
    
    * feat: add conditional styling to hide map controls
    
    Hide map controls based on options.showControls.
    
    * feat: add show controls checkbox with compass icon
    
    Add a checkbox for showing controls with a compass icon.
    
    * fix: map controls handling in Embedding component
    
    Refactor map controls visibility logic and styles.

--- website/src/lib/components/embedding/Embedding.svelte
+++ website/src/lib/components/embedding/Embedding.svelte
@@ -112,7 +112,7 @@
 </script>
 
 <div class="absolute flex flex-col h-full w-full border rounded-xl overflow-clip">
-    <div class="grow relative">
+    <div class="grow relative" class:hide-map-controls={!options.showControls}>
         <Map
             class="h-full {$fileStateCollection.size > 1 ? 'horizontal' : ''}"
             maptilerKey={options.key}
@@ -155,3 +155,9 @@
         </div>
     {/if}
 </div>
+
+<style>
+    .hide-map-controls :global(.maplibregl-ctrl-top-right) {
+        display: none !important;
+    }
+</style>
